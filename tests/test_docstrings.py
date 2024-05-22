fc55cbe20645904c06d6462f564d5e402b026f41
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-05-22 14:46:32 -0700

    updated test_has_docstring to skip modules

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -3,9 +3,9 @@ import pytest
 
 def test_has_docstring(obj):
     """Check if an object has a docstring."""
-    doc = inspect.getdoc(obj)
     if inspect.ismodule(obj):
-        msg = f"{obj.__name__} has no docstring."
-    else:
-        msg = f"{obj.__module__}.{obj.__qualname__} has no docstring."
+        return # modules do not require docstrings
+    
+    doc = inspect.getdoc(obj)
+    msg = f"{obj.__module__}.{obj.__qualname__} has no docstring."
     assert doc is not None, msg
